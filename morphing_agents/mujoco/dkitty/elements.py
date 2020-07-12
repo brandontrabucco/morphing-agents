f5f907ba9b22799c84cf4046a43a7453ff3e43c8
Brandon Trabucco <brandon@btrabucco.com>
2020-07-12 00:25:15 -0700

    update dkitty design default to be in bounds

--- morphing_agents/mujoco/dkitty/elements.py
+++ morphing_agents/mujoco/dkitty/elements.py
@@ -23,8 +23,8 @@ LEG = namedtuple("LEG", [
 
 
 LEG_UPPER_BOUND = LEG(
-    x=0.1,
-    y=0.1,
+    x=0.09,
+    y=0.122,
     z=0,
     a=PI,
     b=PI,
@@ -35,22 +35,22 @@ LEG_UPPER_BOUND = LEG(
     thigh_range=PI / 4,
     ankle_center=PI,
     ankle_range=PI / 4,
-    thigh_size=0.1965,
-    ankle_size=0.1945)
+    thigh_size=0.1365,
+    ankle_size=0.1345)
 
 
 LEG_LOWER_BOUND = LEG(
-    x=-0.1,
-    y=-0.1,
+    x=-0.09,
+    y=-0.122,
     z=0,
-    a=-PI,
-    b=-PI,
-    c=-PI,
+    a=0,
+    b=0,
+    c=0,
     hip_center=-PI,
-    hip_range=0,
+    hip_range=PI / 35,
     thigh_center=-PI,
-    thigh_range=0,
+    thigh_range=PI / 35,
     ankle_center=-PI,
-    ankle_range=0,
+    ankle_range=PI / 35,
     thigh_size=0.0965,
     ankle_size=0.0945)
