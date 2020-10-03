27dd3ef2673a50085d4d63531713a917d4ce82a5
Brandon Trabucco <brandon@btrabucco.com>
2020-10-02 22:50:09 -0700

    update for pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,40 @@
-from setuptools import find_packages
 from setuptools import setup
 
 
+INSTALL_REQUIRES = [
+    'gym',
+    'numpy',
+    'mujoco_py',
+    'robel']
+
+
+CLASSIFIERS = [
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8']
+
+
 setup(
-    name='morphing_agents',
-    version='0.1',
-    description='A Collecting Of Dynamic Morphology Agents',
-    include_package_data=True,
-    packages=find_packages(),
-    install_requires=['gym', 'numpy', 'mujoco_py', 'robel'])
+    name='morphing-agents',
+    packages=['morphing_agents'],
+    version='1.0',
+    license='MIT',
+    description='Collection Of Dynamic Morphology Agents For MuJoCo',
+    author='Brandon Trabucco',
+    author_email='brandon@btrabucco.com',
+    url='https://github.com/brandontrabucco/morphing-agents',
+    download_url='https://github.com/brandontrabucco/design-bench/archive/v1.tar.gz',
+    keywords=['MuJoCo', 'Agents', 'Dynamic Morphology'],
+    install_requires=INSTALL_REQUIRES,
+    classifiers=CLASSIFIERS)
